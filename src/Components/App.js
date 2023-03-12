2654e34455546e9310dd32f04609c86956ceb8e0
Claudia Miranda <clauh9@outlook.pt>
2023-03-12 17:07:44 +0000

    navBar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,29 @@
+import PokemonCard from "./PokemonCard"
+import SearchAppBar from "./SearchAppBar"
+import { Container } from "@mui/system";
+import { Grid } from "@mui/material";
 
 function App() {
   return (
     <>
-      teste
+      <SearchAppBar />
+      <Container maxWidth="false">
+        <Grid container>
+          {/* 3/12 */}
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
---
+++ src/Components/SearchAppBar.jsx
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+// import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+// import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#0971f1',
+      darker: '#053e85',
+    },
+    pokemon: {
+      main: "#ef5350",
+      contrastText: '#fff',
+    },
+  },
+});
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: "#ef5350",
+  '&:hover': {
+    backgroundColor: "rgb(152, 16, 14)",
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: '',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  return (
+    <ThemeProvider theme={theme}>
+    <Box sx={{ flexGrow: 1, marginBottom:"3rem"}}>
+      <AppBar position="static"  color="pokemon" variant="contained">
+      <Toolbar>
+        {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, fontFamily:"Pokemon Solid", color:"yellow", textShadow: "blue 0 0 8px"}}
+          >
+            PoKéDeX
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </ThemeProvider>
+  );
+}
